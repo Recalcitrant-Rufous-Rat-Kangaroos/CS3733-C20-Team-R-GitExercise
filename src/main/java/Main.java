2cfed090955086f2f3a770ee161dee886c048e2e
AstroSeer <matthewnagy@verizon.net>
2020-01-20 18:57:38 -0500

    Matt's First Commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -22,6 +22,8 @@ public class Main{
         System.out.println("Work Please");
         System.out.println("Test line");
         System.out.println("John Wei");
+
+        System.out.println("Matt finally got it");
     }
 
     public void RussellFunct(){
